0b42ba9720882bbcf1e10d30be6487056acdd084
Naresh9313 <nareshprajapati9313@gmail.com>
2025-06-10 20:19:13 +0530

    blog system frontend and backend

--- frontend/src/pages/BlogList/BlogList.js
+++ frontend/src/pages/BlogList/BlogList.js
@@ -1,36 +1,4 @@
 
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import './BlogList.css';
-
-// function BlogList() {
-//   const [blogs, setBlogs] = useState([]);
-
-//   useEffect(() => {
-//     const fetchBlogs = async () => {
-//       const res = await axios.get('http://localhost:5000/getBlogs');
-//       setBlogs(res.data);
-//     };
-//     fetchBlogs();
-//   }, []);
-
-//   return (
-//     <div className="blog-list-container">
-//       <h2>All Blogs</h2>
-//       {blogs.map((blog) => (
-//         <div className="blog-card" key={blog._id}>
-//           <h3>{blog.title}</h3>
-//           <p>{blog.content}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default BlogList;
-
-
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
